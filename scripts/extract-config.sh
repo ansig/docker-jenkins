172064750445b187cb3fe1510cc9158d4b7a9171
Anders Sigfridsson <anders.sigfridsson@gmail.com>
2018-09-30 08:04:28 +0200

    Switch to JCASC for configuration

--- scripts/extract-config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-JENKINS_CONTAINER="$(docker ps -f "name=jenkins-master" -q)"
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-docker cp "${DIR}/config-include.txt" ${JENKINS_CONTAINER}:/tmp/config-include.txt
-docker exec ${JENKINS_CONTAINER} bash -c "tar -czvf /tmp/jenkins-config.tar.gz -C /var/jenkins_home -T /tmp/config-include.txt"
-docker cp ${JENKINS_CONTAINER}:/tmp/jenkins-config.tar.gz jenkins-master/config/default-config.tar.gz
